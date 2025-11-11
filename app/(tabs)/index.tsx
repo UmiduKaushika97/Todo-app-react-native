5bf8ff90570ca46cd2ddc6761176825b0aaed723
UmiduKaushika97 <umidu.kaushika@gmail.com>
2025-11-11 23:50:13 +0400

    todo project create

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const {toggleDarkMode} = useTheme();
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>toggle the mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+  },
+
+  content: {
+    fontSize: 22,
+  }
+      
+});
